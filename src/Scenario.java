afb490ad4e6565e6f64d9db8db7eae0c1e7c50e8
Furkan <furkanakarsu2004@gmail.com>
2026-04-27 21:27:39 +0300

    Update Scenario.java

--- src/Scenario.java
+++ src/Scenario.java
@@ -1,28 +1,37 @@
-
 import java.util.ArrayList;
 
 public class Scenario {
     private String name;
     private Mode mode;
     private ArrayList<Dimension> dimensions;
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public Mode getMode() {
         return mode;
     }
+
     public void setMode(Mode mode) {
         this.mode = mode;
     }
-    public void addDimension(Dimension e){
+
+    public void addDimension(Dimension e) {
         dimensions.add(e);
     }
+
+    public ArrayList<Dimension> getDimensions() {
+        return dimensions;
+    }
+
     public Scenario(String name, Mode mode) {
         this.name = name;
         this.mode = mode;
-        this.dimensions=new ArrayList<>();
+        this.dimensions = new ArrayList<>();
     }
 }
